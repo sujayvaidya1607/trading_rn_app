0e50127adc2ba7d167533fbe0a60940c11822e65
sujayvaidya1607 <123361179+sujayvaidya1607@users.noreply.github.com>
2024-10-28 16:06:14 +0530

    Updated Main

--- src/Components/Common/CommonActions.js
+++
@@ -1,5 +0,0 @@
-import {CommonActions} from '../../Slices/commonSlice';
-
-export const setIsFullScreen = payload => dispatch => {
-  dispatch(CommonActions.setIsFullScreen(payload));
-};
--- src/Components/Routes/routeNames.js
+++
@@ -1,8 +0,0 @@
-const routeNames = {
-  WEEKLY_CALCULATOR_FORM: 'weekly calculator form',
-  WEEKLY_CALCULATOR_RESULT: 'weekly calculator result',
-  YEARLY_CALCULATOR_FORM: 'yearly calculator form',
-  YEARLY_CALCULATOR_RESULT: 'yearly calculator result',
-};
-
-export default routeNames;
